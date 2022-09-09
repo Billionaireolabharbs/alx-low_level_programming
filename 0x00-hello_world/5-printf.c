5be356013a60f68dbbc6e58d0818b22c7992da4a
Babatunde <bharbsolaoluwak@gmail.com>
2022-09-08 21:43:08 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #!/bin/bash
 #include <studio>
-
 /**
  * main - print the string in the put function
  *
@@ -13,4 +12,3 @@ int main(void)
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
-^c
