fc7c37a761562434cb71df3ec046af15afb54caf
Babatunde <bharbsolaoluwak@gmail.com>
2022-10-04 12:20:03 -0700

    str

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,12 +21,13 @@ char *_strdup(char *str)
 	for (index = o; str[index]; index++)
 		len++;
 	cpy = malloc(sizeof(char) * (len + 1));
+	
 	if (cpy == NULL)
 		return (NULL);
 
 	for (index = 0; str [index]; index++)
 	{
-		cpy[index] = str [index];
+		cpy[index] = str[index];
 	}
 
 	cpy[len] = '\o';
